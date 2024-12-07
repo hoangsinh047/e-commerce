d77d93e858081743c5dd7fd3bed0f6eac461f063
hoangsinh047 <leonlike040703@gmail.com>
2024-12-07 22:02:20 +0700

    hdsinh

---
+++ backend/src/main/java/com/mshop/restapi/VNPAYController.java
@@ -0,0 +1,145 @@
+package com.mshop.restapi;
+
+
+import com.mshop.config.PaymentRestDTO;
+import com.mshop.config.VnpayConfig;
+import com.mshop.entity.Order;
+import com.mshop.entity.User;
+import com.mshop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+@RestController
+@RequiredArgsConstructor
+public class VNPAYController {
+
+
+
+    private final UserRepository userRepository;
+
+
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping("/create_payment")
+    @CrossOrigin
+    public ResponseEntity<?> createPayment(@RequestBody Order order) throws IOException, MessagingException {
+
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        String bankCode = "NCB";
+        String vnp_TxnRef = VnpayConfig.getRandomNumber(8);
+        String vnp_IpAddr = "127.0.0.1";
+        String vnp_TmnCode = VnpayConfig.vnp_TmnCode;
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf((int) (order.getAmount() * 100)));
+        vnp_Params.put("vnp_CurrCode", "VND");
+
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bankCode);
+        }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+
+        String locate = "vn";
+        if (locate != null && !locate.isEmpty()) {
+            vnp_Params.put("vnp_Locale", locate);
+        } else {
+            vnp_Params.put("vnp_Locale", "vn");
+        }
+        vnp_Params.put("vnp_ReturnUrl", VnpayConfig.vnp_Returnurl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VnpayConfig.hmacSHA512(VnpayConfig.vnp_HashSecret, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+
+        String paymentUrl = VnpayConfig.vnp_PayUrl  + "?" + queryUrl;
+
+
+        PaymentRestDTO paymentRestDTO = new PaymentRestDTO();
+        paymentRestDTO.setStatus("OK");
+        paymentRestDTO.setMessage("Successfully");
+        paymentRestDTO.setURL(paymentUrl);
+
+        return ResponseEntity.ok(paymentRestDTO);
+    }
+
+    @PostMapping("send-invoice/{id}")
+    private ResponseEntity<?> sendInvoice(@PathVariable Long id) throws MessagingException, IOException {
+        return ResponseEntity.ok("Gửi email hóa đơn thành công");
+    }
+
+    private Long getLongDay(String startDate, String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        LocalDateTime end = LocalDateTime.parse(endDate, formatter);
+        LocalDateTime start = LocalDateTime.parse(startDate, formatter);
+
+        return ChronoUnit.DAYS.between(start.toLocalDate(), end.toLocalDate());
+    }
+
+    public static String readFile(String filename) throws IOException {
+        File file = ResourceUtils.getFile("classpath:" + filename);
+        byte[] encoded = Files.readAllBytes(file.toPath());
+        return new String(encoded, StandardCharsets.UTF_8);
+    }
+
+}
